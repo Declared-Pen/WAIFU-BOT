0a41c24da608eab59d49b70a1ff488e4a6837549
Declared-Pen <bedramshakya07@gmail.com>
2025-04-21 15:57:17 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,7 +5,7 @@ class Config(object):
     OWNER_ID = "7776071513"
     sudo_users = "6312154011", "5559464565"
     GROUP_ID = -1002657854043
-    TOKEN = "7813169206:AAGysB2nOrVjeqdqlLmcg6Cdat84y1TLTs0"
+    TOKEN = "7810386723:AAGbWSJ04yTEjIun8ha93a-t_VK_uMDw6jE"
     mongo_url = "mongodb+srv://itachii130506:idjVit5zIoSOL3Gv@cluster0.vmsab.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
     SUPPORT_CHAT = "Collect_your_waifu"
