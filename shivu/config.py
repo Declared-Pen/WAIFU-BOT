1a592b123e6fafd25239abb96aed863592c8d52c
Declared-Pen <bedramshakya07@gmail.com>
2025-04-21 15:48:22 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -4,14 +4,14 @@ class Config(object):
     # Get this value from my.telegram.org/apps
     OWNER_ID = "7776071513"
     sudo_users = "6312154011", "5559464565"
-    GROUP_ID = -1002374696109
+    GROUP_ID = -1002657854043
     TOKEN = "7813169206:AAGysB2nOrVjeqdqlLmcg6Cdat84y1TLTs0"
     mongo_url = "mongodb+srv://itachii130506:idjVit5zIoSOL3Gv@cluster0.vmsab.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_your_waifu_support"
-    UPDATE_CHAT = "Collect_your_waifu_support"
+    SUPPORT_CHAT = "Collect_your_waifu"
+    UPDATE_CHAT = "Collect_your_waifu"
     BOT_USERNAME = "Collect_your_waifu_bot"
-    CHARA_CHANNEL_ID = "-1002374696109"
+    CHARA_CHANNEL_ID = "-1002657854043"
     api_id = 28682516
     api_hash = "bb43fa00569d8f4de75aad6bdeb3650d"
     
